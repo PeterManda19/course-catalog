fd360dcf4d3604c20a8401a4b80d6dbb1357affb
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-04-20 20:10:57 +0200

    tutorOvertimeWageCalculator

--- tutor-wages/tutorOvertimeWageCalculator.js
+++ tutor-wages/tutorOvertimeWageCalculator.js
@@ -1,19 +1,43 @@
 function tutorOvertimeWageCalculator(timesheet, level) {
-    const hourlyRate = 25 * level; // calculate hourly rate based on tutor level
-    const hoursPerWeek = timesheet.split("-").reduce((total, hours) => Number(total) + Number(hours)); // calculate total hours worked per week
-  
-    let baseWage = 0;
-    let overtimeWage = 0;
-  
-    // calculate base wage and overtime wage based on hours worked
-    if (hoursPerWeek <= 40) {
-      baseWage = hoursPerWeek * hourlyRate;
-    } else if (hoursPerWeek > 40) {
-      baseWage = 40 * hourlyRate; // calculate base wage for first 40 hours
-      overtimeWage = (hoursPerWeek - 40) * (hourlyRate * 1.5); // calculate overtime wage for hours over 40
-    }
-  
-    return baseWage + overtimeWage; // return total wage
+  let totalHours = 0;
+  let overtimeHours = 0;
+  let overtimeRate = 0;
+
+  // split the timesheet string into an array of hours worked for each day
+  const hours = timesheet.split("-");
+
+  // check that the timesheet has exactly 5 or 6 or 7 entries
+  if (hours.length !== 5 && hours.length !== 6 && hours.length !== 7 ) {
+    return 0; // return 0 for invalid timesheet
   }
-  
- 
+  else{
+    // Loop through each number in the array and add it to totalHours
+    for (let i = 0; i < hours.length; i++) {
+      totalHours += Number(hours[i]);
+    }
+  }
+
+  // calculate overtime hours
+  if(totalHours > 40){
+    overtimeHours = totalHours - 40;
+  }
+   
+  // calculate the overtime wage based on the level, overtime hours worked, and overtime rate
+  let overtimeWage = 0;
+  if (level === 1) {
+    hourlyRate = 75;
+    overtimeRate = 0.07; // 7% overtime rate for level 1
+  } else if (level === 2) {
+    hourlyRate = 90;
+    overtimeRate = 0.09; // 9% overtime rate for level 2
+  } else if (level === 3) {
+    hourlyRate = 105;
+    overtimeRate = 0.12; // 12% overtime rate for level 3
+  }
+  if (overtimeHours > 0) {
+    overtimeWage = overtimeHours * (hourlyRate * overtimeRate); // calculate overtime wage
+  }
+
+  // Return the calculated total overtime wage based on extra hours worked
+  return overtimeWage;
+}
