8776436f2858510791898d3de592540ab5f018c3
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-04-20 15:38:09 +0200

    split string and checking for 5 entries

--- tutor-wages/test/tutorWageCalculator.js
+++ tutor-wages/test/tutorWageCalculator.js
@@ -1,3 +1,10 @@
-function tutorWageCalculator(timesheet, level){
-    return 0;
-}
+function tutorWageCalculator(timesheet, level) {
+    // split the timesheet string into an array of hours worked for each day
+    const hours = timesheet.split("-");
+  
+    // check that the timesheet has exactly 5 entries
+    if (hours.length !== 5) {
+      return 0; // return 0 for invalid timesheet
+    }
+  
+  }  
